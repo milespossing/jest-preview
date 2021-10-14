98f1e9e1b74cc9f5bce393a7545f764d2ba7c12c
joey jiron <joeyjiron@microsoft.com>
2021-10-14 21:02:45 +0000

    wip: added dom changed listener

--- test_renderer/env.js
+++ test_renderer/env.js
@@ -2,13 +2,12 @@ const JSDOMEnvironment = require('jest-environment-jsdom-sixteen');
 const writeTestFile = require('./writeTestFile');
 
 function createDiffDom(window) {
-
   global.window = window;
-      global.HTMLElement = window.HTMLElement;
-  
-      const { DiffDOM } = require("diff-dom")
-  
-      return new DiffDOM({ document: window.document });
+  global.HTMLElement = window.HTMLElement;
+
+  const { DiffDOM } = require("diff-dom")
+
+  return new DiffDOM({ document: window.document });
 }
 
 class CustomEnvironment extends JSDOMEnvironment {
@@ -22,21 +21,15 @@ class CustomEnvironment extends JSDOMEnvironment {
     async setup(...other) {
       await super.setup();
 
-
-      const { window } = this.dom;
-
-      global.window = window;
-      global.HTMLElement = window.HTMLElement;
-  
-      const { DiffDOM } = require("diff-dom")
-  
-      this.diffDom = new DiffDOM({ document: window.document });
-
-      // console.log(this.dom);
+      this.diffDom = createDiffDom(window);
+      // connect to to the websocket server
     }
   
     async teardown() {
       await writeTestFile(this.testPath, this.testResultData);
+      
+      // disconnect from the websocket sever
+
       await super.teardown();
     }
   
@@ -50,13 +43,49 @@ class CustomEnvironment extends JSDOMEnvironment {
       if (event.name === 'test_start') {
         const testName = event.test.name;
         const parent = event.test.parent.name;
-        this.testResultData[testName] = { testName, parent, errors: [] };
+        this.testResultData[testName] = { test: event.test, testName, parent, errors: [] };
+
+
+        const { MutationObserver, document } = this.dom.window;
+
+        this.observer = new MutationObserver(() => this.domChanged(state));
+        this.observer.observe(document.body, { subtree: true, childList: true, attributes: true, });
+
+
       } else if (event.name === 'error') {
         this.testResultData[this.currentTest(state)].errors.push(event.error);
       } else if (event.name === 'test_fn_failure' || event.name === 'test_fn_success') {
+        this.observer.disconnect();
+
         this.testResultData[this.currentTest(state)].finalBody = this.dom.window.document.body.innerHTML;
         this.testResultData[this.currentTest(state)].result = event.name === 'test_fn_success' ? 'success' : 'failure';
       }
+    }
+
+    domChanged(state) {
+      const testResultData = this.testResultData[this.currentTest(state)];
+      let { lastHTML } = testResultData;
+      const { document } = this.dom.window;
+      const currentHTML = document.body.innerHTML;
+      let diff;
+
+
+      if (!lastHTML) {
+        lastHTML = '<div></div>';
+      }
+
+      testResultData.lastHTML = currentHTML;
+      const diff = this.diffDom.diff(lastHTML, currentHTML);
+
+      const message = {
+        test: {
+          filename: this.testPath,
+          name: testResultData.testName
+        },
+        diff
+      };
+
+      // TODO: post to websocket
     }
   
     runScript(script) {
