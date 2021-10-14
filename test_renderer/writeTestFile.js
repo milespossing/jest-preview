0cd6e94329c7484a89db5d06b08bdb3a6ac2eb18
Miles Possing <7330275+milespossing@users.noreply.github.com>
2021-10-14 09:08:25 -0700

    Miles/jest preview (#1)
    Adding jest previewer environment

---
+++ test_renderer/writeTestFile.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const { values, omit } = require("lodash");
+
+const outputPath = "__testoutput__";
+
+const cleanPath = (dirtyString) =>
+  dirtyString.replace(/[\/\\|&;$%@"<>()+.,:]/g, "");
+
+const getOutputPath = (fileName) => 
+      outputPath + "\\" + cleanPath(fileName);
+
+const writeTestFile = async (testFileName, testData) => {
+  if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath);
+  const fileData = values(testData).map((testResult) => ({
+    testFileName,
+    ...testResult,
+  }));
+  fileData.forEach((test) => {
+    const fileName = `${test.testFileName}.${test.testName}`;
+    const htmlFile = test.finalBody;
+    const jsonFile = {
+      ...omit(test, "finalBody"),
+      finalHtml: fileName + ".html",
+    };
+    fs.writeFileSync(getOutputPath(fileName) + ".json", JSON.stringify(jsonFile));
+    fs.writeFileSync(getOutputPath(fileName) + ".html", htmlFile);
+  });
+};
+
+module.exports = writeTestFile;
