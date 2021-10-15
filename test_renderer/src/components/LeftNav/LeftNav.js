82ff262820b1c3effa092c139d916cb498585ad5
Miles Possing <7330275+milespossing@users.noreply.github.com>
2021-10-14 17:16:02 -0700

    Forward integration (#3)
    * Create testFiles folder if not present
    
    * CSS updates
    
    * Minor change
    
    Co-authored-by: Sreeram Ganapathy <sganapathy@microsoft.com>

--- test_renderer/src/components/LeftNav/LeftNav.js
+++ test_renderer/src/components/LeftNav/LeftNav.js
@@ -3,24 +3,13 @@ import PropTypes from 'prop-types';
 import { Nav } from 'office-ui-fabric-react/lib/Nav';
 import { FontIcon } from 'office-ui-fabric-react/lib/Icon';
 import styles from './LeftNav.style';
+import { initializeIcons } from '@uifabric/icons';
+import { useHistory } from 'react-router-dom';
 
-const LeftNav = ({ className: classNameProp, ...other }) => {
+const LeftNav = ({ masterFile, className: classNameProp, ...other }) => {
+  const history = useHistory();
 
-  const [masterFile, setMasterFile] = useState(null);
-
-  useEffect(() => {
-    fetchMasterJSON().then(responseJson => {
-      setMasterFile(responseJson);
-    });
-  },[]);
-
-
-  async function fetchMasterJSON() {
-    const response = await fetch('/master.json');
-    const responseJson = await response.json();
-    return responseJson;
-  }
-
+  initializeIcons();
   
   const getLinks = () => {
     var itemRows = [];
@@ -28,25 +17,29 @@ const LeftNav = ({ className: classNameProp, ...other }) => {
       return null;
     }
     masterFile.masterFile.forEach(testCase =>{
+      const iconVal = testCase.result === 'success' ? 'Accept' : 'Cancel';
       const testCaseRows = testCase.renderFileName.map(renderFileName => {
         const filePathArr = renderFileName.split('/');
         const fileName = filePathArr[filePathArr.length-1];
          return {
-          name: fileName,
+          name: fileName.split('testjs').pop(),// hack to show just the test name
           url: `/load/${renderFileName}`, // TODO need to verify path
           key: fileName,
-          className: styles.success
+          icon: iconVal
         };     
       });
 
       itemRows.push({
         name: testCase.name,
         result: testCase.result,
-        links: testCaseRows
+        links: testCaseRows,   
       });
       
     });
-    return itemRows;
+    //return itemRows;
+    return [{
+      links: itemRows
+    }];
   };
 
   const links = getLinks();
@@ -57,11 +50,19 @@ const LeftNav = ({ className: classNameProp, ...other }) => {
       return <h3 className={styles.failure} >{group.name} &#x2717;  </h3>;
   };
 
+  const handleLinkClick = (event, link) => {
+    if (!link.external) {
+      event.preventDefault();
+      history.push(link.url);
+    }
+  };
+
   return (
     <div>
       <Nav
         onRenderGroupHeader={_onRenderGroupHeader}
         groups={links}
+        onLinkClick={handleLinkClick}
       />
     </div>
   );
